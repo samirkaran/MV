e26b8e496394a7c097e2dddaf8b550ad8422903a
samirkaran <samir.karan@sakhatech.com>
2019-12-04 17:38:21 +0530

    Location

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
 
-sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s'
-cd "$JENKINS_HOME\workspace\MarketingVitalP1"
+sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s' < cd "$JENKINS_HOME\workspace\MarketingVitalP1"
 sh MySqlHiveCountusing.sh

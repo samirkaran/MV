08e400fe4e2e2e421c86a97a1e7672a79fcda907
samirkaran <samir.karan@sakhatech.com>
2019-12-04 18:55:15 +0530

    sddf

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 
-/usr/local/bin/sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s' < C:/Users/Samir/.jenkins/workspace/MarketingVitalP1/MySqlHiveCountusing.sh
+C:\cygwin64\usr\local\bin\sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s' < C:/Users/Samir/.jenkins/workspace/MarketingVitalP1/MySqlHiveCountusing.sh

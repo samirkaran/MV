0959800305c7fa039268dafa88c9a72c13f241e1
samirkaran <samir.karan@sakhatech.com>
2019-12-04 12:21:21 +0530

    added $JENKINS_HOME

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-cd "C:\Users\Samir\.jenkins\workspace\MarketingVitalP1"
+cd "$JENKINS_HOME\workspace\MarketingVitalP1"
 
-sshpass -p welcome@123 ssh hdp@175.100.150.91 -o StrictHostKeyChecking=no 'bash -s' < C:\Users\Samir\.jenkins\workspace\MarketingVitalP1\MySqlHiveCountusing.sh
+sshpass -p welcome@123 ssh hdp@175.100.150.91 -o StrictHostKeyChecking=no 'bash -s' < $JENKINS_HOME\workspace\MarketingVitalP1\MySqlHiveCountusing.sh

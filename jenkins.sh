ebbd116f5cc003220a2143a769812491266bdfa5
samirkaran <samir.karan@sakhatech.com>
2019-12-04 12:29:38 +0530

    Location change

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
+sshpass -p welcome@123 ssh hdp@175.100.150.91 -o StrictHostKeyChecking=no 'bash -s' < 
 cd "$JENKINS_HOME\workspace\MarketingVitalP1"
-
-sshpass -p welcome@123 ssh hdp@175.100.150.91 -o StrictHostKeyChecking=no 'bash -s' < /mnt/c/Users/Samir/.jenkins/workspace/MarketingVitalP1/MySqlHiveCountusing.sh
+sh MySqlHiveCountusing.sh

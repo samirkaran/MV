dadbb35bf5706454b11d0a1c02874afdc316732a
samirkaran <samir.karan@sakhatech.com>
2019-12-04 11:27:54 +0530

    added alias

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -xe
 
+alias p='cd C:\Users\Samir\.jenkins\workspace\MarketingVitalP1\'
+
 sshpass -p welcome@123 ssh hdp@175.100.150.91 -o StrictHostKeyChecking=no 'bash -s' < C:\Users\Samir\.jenkins\workspace\MarketingVitalP1\MySqlHiveCountusing.sh

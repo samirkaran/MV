9562a22011b133d90a3221a337ee29468ab80e00
samirkaran <samir.karan@sakhatech.com>
2019-12-04 18:32:58 +0530

    Updated code

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -xe
 
-cd "C:\cygwin64\home\Samir\sshpass-1.06"
-
-sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s' < $JENKINS_HOME/workspace/MarketingVitalP1
-sh MySqlHiveCountusing
+sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s' < $JENKINS_HOME/workspace/MarketingVitalP1/MySqlHiveCountusing

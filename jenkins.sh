0f7b4dc57c6a7863e28d04755f831143e5434706
samirkaran <samir.karan@sakhatech.com>
2019-12-04 17:35:12 +0530

    Change in code

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-sshpass -p welcome@123 ssh hdp@175.100.150.91 -o StrictHostKeyChecking=no 'bash -s'
+sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s'
 cd "$JENKINS_HOME\workspace\MarketingVitalP1"
 sh MySqlHiveCountusing.sh

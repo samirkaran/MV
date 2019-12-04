78cd152347cdc7aaeecb344577612a799181811d
samirkaran <samir.karan@sakhatech.com>
2019-12-04 18:13:40 +0530

    added sshpass path

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -xe
 
+cd "C:\cygwin64\home\Samir\sshpass-1.06"
+
 sshpass -p welcome@123 ssh hdp@175.100.150.91 -p 22 'bash -s' < $JENKINS_HOME/workspace/MarketingVitalP1
 sh MySqlHiveCountusing

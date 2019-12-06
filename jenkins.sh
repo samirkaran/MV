b3cd921ffa2fd10790288f9e3b7a0bb177d06e04
samirkaran <samir.karan@sakhatech.com>
2019-12-06 12:01:24 +0530

    added -tt

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 
-sshpass -p welcome@123 ssh -t hdp@175.100.150.91 -p 22 'bash -s' < 'C:/Users/Samir/.jenkins/workspace/MarketingVitalP1/MySqlHiveCountusing.sh'
+sshpass -p welcome@123 ssh -tt hdp@175.100.150.91 -p 22 'bash -s' < 'C:/Users/Samir/.jenkins/workspace/MarketingVitalP1/MySqlHiveCountusing.sh'
